903ede87ae0c0e15ac83234fe0216c433cadd562
nguyenducminh <nguyenducminh01122002@gmail.com>
2023-12-02 19:36:32 +0700

    update: Chia role cho ae. Ae đọc đk cái update này thì chọn role r báo lại trên nhóm nhá

--- lib/modules/main/main_screen.dart
+++ lib/modules/main/main_screen.dart
@@ -29,7 +29,7 @@ class MainScreen extends GetWidget<MainController> {
             BottomNavyBarItem(
               icon: const Icon(Icons.home_rounded),
               title: Text(
-                'Home',
+                'Dashboard', //Có thông tin cơ bản con chó, rồi mấy cái số liệu ba xàm ba láp cho vào đây
                 style: GoogleFonts.openSans(
                   fontSize: 10.sp,
                   fontWeight: FontWeight.bold,
@@ -39,13 +39,24 @@ class MainScreen extends GetWidget<MainController> {
             BottomNavyBarItem(
               icon: const Icon(Icons.home_rounded),
               title: Text(
-                'Home',
+                'Schedule', //Có mấy cái lịch ăn uống rồi suggest món j j đấy. Ae nghĩ cái tên j hay hay bỏ vào nhá
+                style: GoogleFonts.openSans(
+                  fontSize: 10.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            BottomNavyBarItem(
+              icon: const Icon(Icons.home_rounded),
+              title: Text(
+                'Blog', //Cái bài đăng á, cái này vào đọc đk nếu có mạng, muốn đăng bài thì force ra đăng nhập
                 style: GoogleFonts.openSans(
                   fontSize: 10.sp,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             )
+            //Ae ai có j muốn thêm thì góp ý nhá
           ],
         ),
       ),
